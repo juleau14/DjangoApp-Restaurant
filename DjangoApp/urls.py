adbb3fd5e0cc855bbab70e68baac537a0ff5db35
Jules CAMUZET <jcamuzet@yahoo.com>
2022-10-12 11:58:31 +0200

    airdefamille -> DjangoApp

--- DjangoApp/urls.py
+++ DjangoApp/urls.py
@@ -1,4 +1,4 @@
-"""airdefamille URL Configuration
+"""DjangoApp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
