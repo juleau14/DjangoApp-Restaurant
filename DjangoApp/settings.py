a8be8d0c98051bb3e6877385ffb6b15115aadbc7
Jules CAMUZET <jcamuzet@yahoo.com>
2022-10-17 17:02:36 +0200

    yahoo -> outlook

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -126,11 +126,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL  = '/login/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'mail.yahoo.com'
+EMAIL_HOST = 'office365.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 #EMAIL_USE_SSL = True
-EMAIL_HOST_USER = 'test_django@yahoo.com'
+EMAIL_HOST_USER = 'test_django@outlook.fr'
 EMAIL_HOST_PASSWORD = 'JuleauTestdjango@10108'
 # EMAIL_FILE_PATH = 'website/mails_files/' # change this to a proper location
 
