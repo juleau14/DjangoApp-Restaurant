7f346ec453a0ac62631afded130b10659a0f683c
Jules CAMUZET <jcamuzet@yahoo.com>
2022-11-11 17:26:05 +0100

    open and close phone nav

--- website/static/website/scripts/home_script.js
+++ website/static/website/scripts/home_script.js
@@ -1,5 +1,8 @@
 const body = document.querySelector("body");
 
+const openPhoneNavButton = document.querySelector(".open_phone_nav");
+const closePhoneNavButton = document.querySelector(".close_phone_nav");
+
 const contactsScrollButton = document.querySelector(".nav.contacts");
 const pricesScrollButton = document.querySelector(".nav.prices");
 const topScrollButton = document.querySelector(".nav.top");
@@ -8,6 +11,11 @@ const topScrollButton = document.querySelector(".nav.top");
 function start() {
     // Loader
     window.addEventListener("load", hideLoader);
+
+    // PhoneNav
+    window.addEventListener("scroll", phoneNavButtonToTop);
+    openPhoneNavButton.addEventListener("click", displayPhoneNav);
+    closePhoneNavButton.addEventListener("click", closePhoneNav);
 
     // Progress bar
     window.addEventListener("scroll", progressBar);     
@@ -49,6 +57,40 @@ function progressBar() {
     let barWidth = ratioAlreadyScrolled * totalWidth;
 
     bar.style.width = String(barWidth) + "px";
+}
+
+
+function phoneNavButtonToTop() {
+    if (window.scrollY == 0) {
+        openPhoneNavButton.style.marginTop = '0';
+    }
+    else {
+        openPhoneNavButton.style.marginTop = '8px';
+    }
+}
+
+
+function displayPhoneNav() {
+    const phoneNav = document.querySelector(".phone_nav");
+
+    phoneNav.style.visibility = 'inherit';
+    phoneNav.style.transform = 'rotate(0)';
+    phoneNav.style.height = '100vh';
+    phoneNav.style.marginLeft = '0';
+
+    openPhoneNavButton.style.marginLeft = '-70px';
+}
+
+
+function closePhoneNav() {
+    const phoneNav = document.querySelector(".phone_nav");
+
+    phoneNav.style.marginLeft = '-200vw';
+    phoneNav.style.transform = 'rotate(-45deg)';
+    phoneNav.style.height = '400vh';
+    setTimeout(function() {phoneNav.style.visibility = 'hidden'}, 1000);
+
+    openPhoneNavButton.style.marginLeft = '0';
 }
 
 
